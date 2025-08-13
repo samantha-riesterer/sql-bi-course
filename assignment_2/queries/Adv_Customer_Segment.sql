46b1c8e92f023ab081406f62095ea9f54708b0e0
Samantha Riesterer <riesterersamantha@gmail.com>
2025-08-13 16:55:58 -0700

    Adv. Customer Segmentation file setup and structure

---
+++ assignment_2/queries/Adv_Customer_Segment.sql
@@ -0,0 +1,37 @@
+-- =====================================================================================================================
+-- PART 2 : ADVANCED CUSTOMER SEGMENTATION
+-- Purpose: Use advanced customer analytics to identify high-value segments, udnerstand retention patterns and optimize
+-- marketing spend allocation
+-- Analyst: Samantha Riesterer
+-- ====================================================================================================================
+
+-- ================================================================================= --
+-- QUESTION 1: RFM Analysis                                                          --
+-- Address:How should we segmenet our customers for targeted campaigns?              --         
+-- ================================================================================= --
+
+/*******************************************************************************************************************
+    Q  : 
+    
+    Notes: 
+********************************************************************************************************************/
+
+
+
+
+
+
+
+
+-- ================================================================================= --
+-- QUESTION 2: Customer Cohort Analysis                                              --
+-- Address:How are customers retained over time, and which acquisition periods had   --
+-- best retention                                                                    --
+-- ================================================================================= --
+
+
+-- ================================================================================= --
+-- QUESTION 3: Customer Lifetime Value Modeling                                      --
+-- Address:What is the predicted lifetime value of different customer segments?      --
+-- ================================================================================= --
+
